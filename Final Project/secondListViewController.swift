75aa568a54f4032b1d7305ddab05fb6d6aa68c07
Sequoia Taylor <sequoiataylor123@gmail.com>
2021-07-20 22:26:32 -0700

    Made the buttons functional and made the list of activities a screen. (Still thinking about how to implement the specific activity screens, but I think after some sleep I'll get some inspiration) - Sequoia

---
+++ Final Project/secondListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  secondListViewController.swift
+//  Final Project
+//
+//  Created by Sequoia Taylor on 7/20/21.
+//
+
+import UIKit
+
+class secondListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
